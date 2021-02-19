bafa4456518b1bb85a6707948da6f076e7d752af
Suat Ozcan <41187131+SuatOzcan@users.noreply.github.com>
2021-02-19 19:14:44 +0300

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int curClicks;
+    public static GameManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public void AddClicks(int amount)
+    {
+        curClicks += amount;
+        UI.instance.UpdateClickText();
+    }
+}
---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    public TextMeshProUGUI clickText;
+    public Image clickButtonImage;
+    public Sprite[] buttonSprites;
+    public static UI instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+    public void OnClickButton()
+    {
+        GameManager.instance.AddClicks(1);
+        clickButtonImage.sprite = buttonSprites[Random.Range(0, buttonSprites.Length)];
+    }
+    public void UpdateClickText()
+    {
+        clickText.text = GameManager.instance.curClicks.ToString();
+    }
+}
